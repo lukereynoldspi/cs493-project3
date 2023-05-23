fedefc2985e655a658de63ea75859e91f9c91521
lukereynoldspi <lukereynoldspi@gmail.com>
2023-05-22 23:42:26 -0700

    added password hashing

--- models/user.js
+++ models/user.js
@@ -6,7 +6,11 @@ const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, autoIncrement: true,primaryKey: true, allowNull: false},
     username: {type: DataTypes.STRING, allowNull: false },
     email: {type: DataTypes.STRING, allowNull: false, unique: true},
-    password: {type: DataTypes.STRING, allowNull: false},
+    password: {type: DataTypes.STRING, allowNull: false, set(value) {
+        // Storing passwords in plaintext in the database is terrible.
+        // Hashing the value with an appropriate cryptographic hash function is better.
+        this.setDataValue('password', hash(value));
+      }},
     admin: {type: DataTypes.STRING, allowNull: false}
 })
 
